3d0ff9f75818e3f967ef7892cc052e086207c346
David Lewis <dclewiss17@gmail.com>
2025-07-16 21:50:12 -0700

    Home manager module added. Importing home-base and home-niri.

--- niri-flake/flake.nix
+++ niri-flake/flake.nix
@@ -3,18 +3,43 @@
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.05";
+    home-manager = {
+      url = "github:nix-community/home-manager/release-25.05";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
   outputs =
-    { self, nixpkgs, ... }@inputs:
+    {
+      self,
+      nixpkgs,
+      home-manager,
+      ...
+    }@inputs:
     {
       # System-level config
       nixosConfigurations.nixpad = nixpkgs.lib.nixosSystem {
         system = "x86_64-linux";
         modules = [
-          ./system.nix
+          # Base sys config (not env specific)
           ../configuration.nix
+          # ENV specific sys config
+          ./system.nix
+          # Extra Modules
           ./modules/sddm.nix
+
+          home-manager.nixosModules.home-manager
+          {
+            home-manager.useGlobalPkgs = true;
+            home-manager.useUserPackages = true;
+
+            home-manager.users.david = {
+              imports = [
+                ../shared/home-base.nix
+                ./home-niri.nix
+              ];
+            };
+          }
         ];
       };
     };
