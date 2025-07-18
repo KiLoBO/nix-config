22db4b9f7f31c29d569f5fb10857caab86d56b2a
David Lewis <dclewiss17@gmail.com>
2025-07-18 14:42:53 -0700

    Theming. CPT does not support GTK i guess.

--- shared/home-base.nix
+++ shared/home-base.nix
@@ -98,7 +98,7 @@
     enable = true;
     font.name = "TeX Gyre Adventor 10";
     theme = {
-      name = "Juno";
+      name = "Juno-mirage";
       package = pkgs.juno-theme;
     };
     iconTheme = {
@@ -119,23 +119,6 @@
   programs.fzf = {
     enable = true;
     enableZshIntegration = true;
-    # colors = {
-    #   "bg+" = "#313244";
-    #   "bg" = "#1E1E2E";
-    #   "spinner" = "#F5E0DC";
-    #   "hl" = "#F38BA8";
-    #   "fg" = "#CDD6F4";
-    #   "header" = "#F38BA8";
-    #   "info" = "#CBA6F7";
-    #   "pointer" = "#F5E0DC";
-    #   "marker" = "#B4BEFE";
-    #   "fg+" = "#CDD6F4";
-    #   "prompt" = "#CBA6F7";
-    #   "hl+" = "#F38BA8";
-    #   "selected-bg" = "#45475A";
-    #   "border" = "#313244";
-    #   "label" = "#CDD6F4";
-    # };
   };
 
   programs.starship.enable = true;
