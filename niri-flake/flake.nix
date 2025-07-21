26834a57243da77135c90043f0a39ec7290346a3
David Lewis <dclewiss17@gmail.com>
2025-07-21 11:54:12 -0700

    Adding niri module.

--- niri-flake/flake.nix
+++ niri-flake/flake.nix
@@ -7,7 +7,10 @@
       url = "github:nix-community/home-manager/release-25.05";
       inputs.nixpkgs.follows = "nixpkgs";
     };
-
+    niri = {
+      url = "github:sodiboo/niri-flake";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
     catppuccin.url = "github:catppuccin/nix";
   };
 
@@ -17,20 +20,27 @@
       nixpkgs,
       home-manager,
       catppuccin,
+      niri,
       ...
     }@inputs:
     {
       # System-level config
       nixosConfigurations.nixpad = nixpkgs.lib.nixosSystem {
         system = "x86_64-linux";
+        specialArgs = { inherit inputs; };
         modules = [
+          {
+            nixpkgs.overlays = [
+              niri.overlays.niri
+            ];
+          }
           # Base sys config (not env specific)
           ../configuration.nix
           # ENV specific sys config
           ./system.nix
           # Extra Modules
           ./modules/sddm.nix
-
+          niri.nixosModules.niri
           catppuccin.nixosModules.catppuccin
           {
             catppuccin.enable = true;
@@ -48,7 +58,6 @@
               imports = [
                 ../shared/home-base.nix
                 ./home-niri.nix
-
                 catppuccin.homeModules.catppuccin
                 ../shared/themes/catppuccin
               ];
