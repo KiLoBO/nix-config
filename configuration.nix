aa619289871d2ecb3d3915e386ea8a6f4e7ce92a
David Lewis <dclewiss17@gmail.com>
2025-09-02 15:34:00 -0700

    load iptable_nat module

--- configuration.nix
+++ configuration.nix
@@ -14,6 +14,7 @@
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
   boot.loader.systemd-boot.configurationLimit = 5;
+  boot.kernelModules = [ "iptable_nat" ];
 
   networking.hostName = "nixpad"; # Define your hostname.
   # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
