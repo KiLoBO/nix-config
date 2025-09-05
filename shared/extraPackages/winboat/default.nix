d639b45e6d4d38b869cd932fd19512422035dc47
David Lewis <dclewiss17@gmail.com>
2025-09-05 14:06:38 -0700

    RMV icedos

--- shared/extraPackages/winboat/default.nix
+++ shared/extraPackages/winboat/default.nix
@@ -29,25 +29,6 @@
           ];
 
           virtualisation.docker.enable = true;
-
-          icedos.applications.toolset.commands = [
-            (
-              let
-                command = "clear-winboat";
-                docker = "${pkgs.docker}/bin/docker";
-              in
-              {
-                bin = "${pkgs.writeShellScript command ''
-                  ${docker} stop WinBoat
-                  ${docker} rm WinBoat
-                  ${docker} volume rm winboat_data
-                  rm -rf ~/.winboat
-                ''} 2> /dev/null";
-                command = command;
-                help = "purge all winboat files for a clean installation";
-              }
-            )
-          ]; 
         }
       )
     ];
