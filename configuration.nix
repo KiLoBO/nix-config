ec6c7153844ae9ce47b2ec50724740ff52a4b159
David Lewis <dclewiss17@gmail.com>
2025-10-30 15:46:05 -0700

    enable jack

--- configuration.nix
+++ configuration.nix
@@ -65,6 +65,7 @@
     alsa.enable = true;
     alsa.support32Bit = true;
     pulse.enable = true;
+    jack.enable = true;
     # If you want to use JACK applications, uncomment this
     #jack.enable = true;
 
